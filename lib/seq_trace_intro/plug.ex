8a56f5a3e47bf3a7bf12ddc03c2a5efaedc682ff
Vince Foley <vincefoley@gmail.com>
2019-05-29 22:52:26 -0700

    demo of seq_trace alongside telemetry

--- lib/seq_trace_intro/plug.ex
+++ lib/seq_trace_intro/plug.ex
@@ -1,16 +1,33 @@
 defmodule SeqTraceIntro.Plug do
   use Plug.Router
 
-  plug(Plug.Telemetry, event_prefix: [:seq_trace_intro])
+  plug(Plug.Telemetry, event_prefix: [:seq_trace_intro, :plug])
 
   plug(:match)
   plug(:dispatch)
 
-  get "/hello" do
-    send_resp(conn, 200, "world")
-  end
+  # Erlang's `seq_trace` module can be used to achieve cross-process
+  # trace context propagation by leveraging it's `label` feature
+  #
+  #   http://erlang.org/doc/man/seq_trace.html
+  #
 
-  match _ do
-    send_resp(conn, 404, "oops")
+  get "/trace" do
+    Task.async(fn ->
+      Task.async(fn ->
+        # The Sequential Trace `label` is propagated for every message
+        # `Task` executes it's function via message passing, so we can grab it:
+
+        case :seq_trace.get_token(:label) do
+          {:label, {:trace_id, trace_id}} ->
+            :telemetry.execute([:task, :trace_id], %{trace_id: trace_id})
+
+          _ ->
+            :ignore
+        end
+      end)
+    end)
+
+    send_resp(conn, 200, "traced!")
   end
 end
--- test/seq_trace_intro_test.exs
+++ test/seq_trace_intro_test.exs
@@ -2,8 +2,33 @@ defmodule SeqTraceIntroTest do
   use ExUnit.Case
   doctest SeqTraceIntro
 
-  test "greets the world" do
-    :httpc.request('http://localhost:4001/hello')
-    |> IO.inspect()
+  test "Cross process Trace ID propigation" do
+    trace_id = :rand.uniform() |> inspect()
+    test_pid = self()
+
+    :telemetry.attach_many(
+      :seq_trace_intro,
+      [
+        [:seq_trace_intro, :plug, :start],
+        [:task, :trace_id]
+      ],
+      fn
+        [:seq_trace_intro, :plug, :start], _measurements, _metadata, _config ->
+          # Start a Sequential Trace inside a `telemetry` handler
+          # by setting a `label`, which can be any term:
+
+          :seq_trace.set_token(:label, {:trace_id, trace_id})
+
+        [:task, :trace_id] = event, measurements, metadata, config ->
+          send(test_pid, {event, measurements, metadata, config})
+      end,
+      %{}
+    )
+
+    :httpc.request('http://localhost:4001/trace')
+
+    # The Trace ID was available inside the nested Task!
+    assert_receive {[:task, :trace_id], %{trace_id: task_trace_id}, _, _}
+    assert trace_id == task_trace_id
   end
 end
