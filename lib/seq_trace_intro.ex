08e5558a8a6312856d84b0b46d9befb81a8ddd39
Vince Foley <vincefoley@gmail.com>
2019-05-29 21:28:41 -0700

    init

---
+++ lib/seq_trace_intro.ex
@@ -0,0 +1,18 @@
+defmodule SeqTraceIntro do
+  @moduledoc """
+  Documentation for SeqTraceIntro.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SeqTraceIntro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/seq_trace_intro_test.exs
@@ -0,0 +1,8 @@
+defmodule SeqTraceIntroTest do
+  use ExUnit.Case
+  doctest SeqTraceIntro
+
+  test "greets the world" do
+    assert SeqTraceIntro.hello() == :world
+  end
+end
